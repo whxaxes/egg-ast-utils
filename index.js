0580f3c39cd7652995892f4717ee9ce66af5c78e
wanghx <whx89768@alibaba-inc.com>
2017-11-04 14:42:15 +0800

    feat: change object position

--- index.js
+++ index.js
@@ -4,13 +4,10 @@ const path = require('path');
 const utils = require('./lib/utils');
 
 module.exports = class AstUtils {
-  constructor({ baseDir }) {
-    this.baseDir = baseDir;
+  constructor() {
     const libDir = path.resolve(__dirname, './lib/finder');
     utils.findFiles(libDir).forEach(fp => {
-      const method = require(fp);
-      this[path.basename(fp, '.js')] = key =>
-        method.call(this, key, this.baseDir);
+      this[path.basename(fp, '.js')] = require(fp);
     });
   }
 };
--- test/index.test.js
+++ test/index.test.js
@@ -3,15 +3,32 @@
 const AstUtils = require('../');
 const path = require('path');
 const assert = require('assert');
+const utils = require('../lib/utils');
+const fs = require('fs');
+const recast = require('recast');
+const baseDir = path.resolve(__dirname, './fixtures/app1');
+const astUtil = new AstUtils();
+
+function findAllConfig(key) {
+  const fileMaps = {};
+  utils.findFiles(path.resolve(baseDir, './config')).forEach(fp => {
+    const result = astUtil.findConfig(fs.readFileSync(fp).toString(), key);
+    if (result.node) {
+      result.text = recast.print(result.node).code;
+      fileMaps[fp] = result;
+    }
+  });
+  return fileMaps;
+}
 
 describe('test/index.test.js', () => {
-  const util = new AstUtils({
-    baseDir: path.resolve(__dirname, './fixtures/app1'),
-  });
-
   it('should work correctly', () => {
-    const result = util.findConfig('view.mapping[".nj"]');
+    const result = findAllConfig('view.mapping[".nj"]');
     const list = Object.keys(result);
+    list.forEach(item => {
+      assert(result[item].text === '\'.nj\'');
+    });
+
     assert(list.includes(path.resolve(__dirname, './fixtures/app1/config/config.default.js')));
     assert(list.includes(path.resolve(__dirname, './fixtures/app1/config/config.local.js')));
     assert(list.includes(path.resolve(__dirname, './fixtures/app1/config/config.prod.js')));
@@ -19,7 +36,7 @@ describe('test/index.test.js', () => {
   });
 
   it('should not match config while it was not exist', () => {
-    const result = util.findConfig('sfclient.serverList[0]');
+    const result = findAllConfig('sfclient.serverList[0]');
     const list = Object.keys(result);
     list.forEach(item => {
       assert(result[item].text === '\'asdasdasdasd\'');
@@ -32,16 +49,16 @@ describe('test/index.test.js', () => {
   });
 
   it('should has config position', () => {
-    let result = util.findConfig('keys');
+    let result = findAllConfig('keys');
     const list = Object.keys(result);
 
-    assert(result[list[0]].loc.start.line === 7);
+    assert(result[list[0]].node.loc.start.line === 7);
     assert(list.includes(path.resolve(__dirname, './fixtures/app1/config/config.default.js')));
     assert(!list.includes(path.resolve(__dirname, './fixtures/app1/config/config.local.js')));
     assert(!list.includes(path.resolve(__dirname, './fixtures/app1/config/config.prod.js')));
     assert(!list.includes(path.resolve(__dirname, './fixtures/app1/config/config.unittest.js')));
 
-    result = util.findConfig('view.defaultViewEngine');
-    assert(result[list[0]].loc.start.line === 11);
+    result = findAllConfig('view.defaultViewEngine');
+    assert(result[list[0]].node.loc.start.line === 11);
   });
 });
