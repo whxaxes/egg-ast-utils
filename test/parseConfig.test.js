1386b57bc614bb75e068822cf19e3ef6d7af7485
wanghx <whx89768@alibaba-inc.com>
2017-11-16 22:58:56 +0800

    test: complete unittest

--- test/parseConfig.test.js
+++ test/parseConfig.test.js
@@ -4,34 +4,35 @@ const path = require('path');
 const fs = require('fs');
 const assert = require('assert');
 const utils = require('../');
-const util = require('./util');
+const getValue = require('../lib/utils').getValue;
 const baseDir = path.resolve(__dirname, './fixtures/app1');
 
 describe('test/parseNormal.test.js', () => {
   it('should work correctly with config', () => {
     let url = path.resolve(baseDir, './config/config.default.js');
     let result = utils.parseConfig(fs.readFileSync(url).toString());
-    console.log(result.find('view'));
     assert(result.find('view').length === 2);
+    assert(result.find('view.bbc').length === 1);
     assert(result.find('view.mapping[".nj"]').length === 2);
-    // console.log(result.children.static[1].children.maxAge[0]);
-    // console.log(result.find('view.mapping[".nj"]'));
-    // console.log(result.find('static.maxAge', true));
-
-    // util.matchNode(url, result);
-    // assert(result.find('view.mapping[".nj"]').nodes.length === 2);
-    // assert(Object.keys(result.get('view').children).length === 2);
+    assert(result.find('static.maxAge').length === 2);
+    assert(getValue(result.find('static.maxAge.aaa')[0].key) === 'aaa');
+    assert(getValue(result.find('static.maxAge.aaa')[0].value) === '123');
+    assert(getValue(result.find('view.mapping[".nj"]')[0].value) === 'nunjucks');
+    assert(getValue(result.find('view.mapping[".nj"]')[1].value) === '111');
 
     url = path.resolve(baseDir, './config/config.local.js');
     result = utils.parseConfig(fs.readFileSync(url).toString());
-    // util.matchNode(url, result);
+    assert(getValue(result.find('sfclient.alias.gh')[0].value) === 'asdasdasdasd');
+    assert(getValue(result.find('sfclient.serverList[0]')[0].value) === 'asdasdasdasd');
 
-    // url = path.resolve(baseDir, './config/config.prod.js');
-    // result = utils.parseConfig(fs.readFileSync(url).toString());
-    // util.matchNode(url, result);
+    url = path.resolve(baseDir, './config/config.prod.js');
+    result = utils.parseConfig(fs.readFileSync(url).toString());
+    assert(getValue(result.find('sfclient.alias.gh')[0].value) === 'asdasdasdasd');
+    assert(getValue(result.find('sfclient.serverList[0]')[0].value) === 'asdasdasdasd');
 
-    // url = path.resolve(baseDir, './config/config.unittest.js');
-    // result = utils.parseConfig(fs.readFileSync(url).toString());
-    // util.matchNode(url, result);
+    url = path.resolve(baseDir, './config/config.unittest.js');
+    result = utils.parseConfig(fs.readFileSync(url).toString());
+    assert(getValue(result.find('sfclient.alias.gh')[0].value) === 'asdasdasdasd');
+    assert(getValue(result.find('sfclient.serverList[0]')[0].value) === 'asdasdasdasd');
   });
 });
